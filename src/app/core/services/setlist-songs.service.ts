08bacfe7ead5060686bdf895c14895aa52288dad
Tim Burnham <tim.burnham@broadcom.com>
2024-01-30 08:53:47 -0500

    Rename reorder setlist song function and formating setlist song list and service.

--- src/app/core/services/setlist-songs.service.ts
+++ src/app/core/services/setlist-songs.service.ts
@@ -1,105 +1,141 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Observable, concat, concatMap, from, map } from 'rxjs';
-import { Song } from '../model/song';
-import { SetlistSong, SetlistSongHelper } from '../model/setlist-song';
-import { SetlistBreak, SetlistBreakHelper } from '../model/setlist-break';
-import { BaseUser } from '../model/user';
-import { convertSnaps } from './db-utils';
+import { Injectable } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from "@angular/fire/compat/firestore";
+import { Observable, concat, concatMap, from, map } from "rxjs";
+import { Song } from "../model/song";
+import { SetlistSong, SetlistSongHelper } from "../model/setlist-song";
+import { SetlistBreak, SetlistBreakHelper } from "../model/setlist-break";
+import { BaseUser } from "../model/user";
+import { convertSnaps } from "./db-utils";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SetlistSongsService {
-
-  constructor(private db: AngularFirestore) {
-    
-  }
+  constructor(private db: AngularFirestore) {}
 
   getSetlistSongs(accountId: string, setlistId: string): Observable<any> {
     const dbPath = `/accounts/${accountId}/setlists/${setlistId}/songs`;
-    const songsRef = this.db.collection(dbPath, ref => ref.orderBy("sequenceNumber"));
+    const songsRef = this.db.collection(dbPath, (ref) =>
+      ref.orderBy("sequenceNumber")
+    );
     return songsRef.snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c =>
-          {
-            const song = c.payload.doc.data() as SetlistSong;
-            song.id = c.payload.doc.id;
-            return song;
-          }
-        )
+      map((changes) =>
+        changes.map((c) => {
+          const song = c.payload.doc.data() as SetlistSong;
+          song.id = c.payload.doc.id;
+          return song;
+        })
       )
     );
   }
 
-  addSetlistBreak(accountId: string, setlistId: string, setlistSong: SetlistBreak): any {
-    const songForAdd = SetlistBreakHelper.getSetlistBreakForAddOrUpdate(setlistSong);
+  addSetlistBreak(
+    accountId: string,
+    setlistId: string,
+    setlistSong: SetlistBreak
+  ): any {
+    const songForAdd =
+      SetlistBreakHelper.getSetlistBreakForAddOrUpdate(setlistSong);
     const dbPath = `/accounts/${accountId}/setlists/${setlistId}/songs`;
     const setlistSongsRef = this.db.collection(dbPath);
-    
+
     return setlistSongsRef.add(songForAdd);
   }
 
-  addSetlistSong(accountId: string, setlistId: string, setlistSong: SetlistSong, editingUser: BaseUser): any {
+  addSetlistSong(
+    accountId: string,
+    setlistId: string,
+    setlistSong: SetlistSong,
+    editingUser: BaseUser
+  ): any {
     const songForAdd = SetlistSongHelper.getForUpdate(setlistSong, editingUser);
     const dbPath = `/accounts/${accountId}/setlists/${setlistId}/songs`;
     const setlistSongsRef = this.db.collection(dbPath);
-    
+
     return setlistSongsRef.add(songForAdd);
   }
 
-  updateSetlistSong(setlistSongId: string,accountId: string, setlistId: string, setlistSong: SetlistSong, editingUser: BaseUser): any {
-    const setlisSongForUpdate = SetlistSongHelper.getForUpdate(setlistSong, editingUser);
+  updateSetlistSong(
+    setlistSongId: string,
+    accountId: string,
+    setlistId: string,
+    setlistSong: SetlistSong,
+    editingUser: BaseUser
+  ): any {
+    const setlisSongForUpdate = SetlistSongHelper.getForUpdate(
+      setlistSong,
+      editingUser
+    );
     const dbPath = `/accounts/${accountId}/setlists/${setlistId}/songs`;
     const setlistSongsRef = this.db.collection(dbPath);
-    
+
     return from(setlistSongsRef.doc(setlistSongId).update(setlisSongForUpdate));
   }
 
-  //Batch commit incrementing the setlist song sequence number.
-  insertSetlistSong(setlistSongToInsert, startingSequenceNumber: number, accountId: string, setlistId: string, editingUser: BaseUser): any {
+  //When you move up a setlist songs all songs below need to be reordered.
+  moveUpSetlistSong(
+    setlistSongToInsert,
+    startingSequenceNumber: number,
+    accountId: string,
+    setlistId: string,
+    editingUser: BaseUser
+  ): any {
     const dbPath = `/accounts/${accountId}/setlists/${setlistId}/songs`;
     const setlistSongsRef = this.db.collection(dbPath);
-    return this.getSetlistSongs(accountId, setlistId)
-                    .pipe(
-                      concatMap(results => {
-                          console.log('insert setlist song')
-                          const setlistSongs = results;
-                          const batch = this.db.firestore.batch();
-                          //Once the starting sequence number is found set this to true so the others can be incremented after.
-                          let isUpdating = false;
-                          let index = 1;
-                          for(const setlistSong of setlistSongs){
-                            //Increment the sequence number if the sequenceNumber === startingSequenceNumber or
-                            // it is updating.
-                            if(index === startingSequenceNumber || (isUpdating && setlistSong.id !== setlistSongToInsert.id)){
-                              this.setSetlistSongSequenceNumberForBatch(setlistSong.sequenceNumber += 1, 
-                                                                        setlistSongsRef, 
-                                                                        setlistSong, 
-                                                                        editingUser, 
-                                                                        batch);
+    return this.getSetlistSongs(accountId, setlistId).pipe(
+      concatMap((results) => {
+        const setlistSongs = results;
+        const batch = this.db.firestore.batch();
+        //Once the starting sequence number is found set this to true so the others can be incremented after.
+        let isUpdating = false;
+        let index = 1;
+        for (const setlistSong of setlistSongs) {
+          //Increment the sequence number if the sequenceNumber === startingSequenceNumber or it is updating.
+          if (
+            index === startingSequenceNumber ||
+            (isUpdating && setlistSong.id !== setlistSongToInsert.id)
+          ) {
+            this.setSetlistSongSequenceNumberForBatch(
+              (setlistSong.sequenceNumber += 1),
+              setlistSongsRef,
+              setlistSong,
+              editingUser,
+              batch
+            );
 
-                              isUpdating = true;
-                            }
-                            else if(isUpdating && setlistSong.id === setlistSongToInsert.id){
-                              this.setSetlistSongSequenceNumberForBatch(setlistSong.sequenceNumber = startingSequenceNumber, 
-                                setlistSongsRef, 
-                                setlistSong, 
-                                editingUser, 
-                                batch);
-                            }
-                            index++;
-                          }
-                          
-                          return from(batch.commit());
-                        }
-                      )
-                    );
+            isUpdating = true;
+          } else if (isUpdating && setlistSong.id === setlistSongToInsert.id) {
+            this.setSetlistSongSequenceNumberForBatch(
+              (setlistSong.sequenceNumber = startingSequenceNumber),
+              setlistSongsRef,
+              setlistSong,
+              editingUser,
+              batch
+            );
+          }
+          index++;
+        }
+        //Batch commit incrementing the setlist song sequence number.
+        return from(batch.commit());
+      })
+    );
   }
 
-  private setSetlistSongSequenceNumberForBatch(sequenceNumber: number, setlistSongsRef: AngularFirestoreCollection<unknown>, setlistSong: SetlistSong, editingUser: BaseUser, batch) {
+  private setSetlistSongSequenceNumberForBatch(
+    sequenceNumber: number,
+    setlistSongsRef: AngularFirestoreCollection<unknown>,
+    setlistSong: SetlistSong,
+    editingUser: BaseUser,
+    batch
+  ) {
     const setlistSongRef = setlistSongsRef.doc(setlistSong.id).ref;
-    const setlisSongForUpdate = SetlistSongHelper.getForUpdate(setlistSong, editingUser)
+    const setlisSongForUpdate = SetlistSongHelper.getForUpdate(
+      setlistSong,
+      editingUser
+    );
     setlisSongForUpdate.sequenceNumber = sequenceNumber;
     setlisSongForUpdate.lastUpdatedByUser = editingUser;
     batch.update(setlistSongRef, setlisSongForUpdate);
--- src/app/features/setlists/setlist-songs-list/setlist-songs-list.component.ts
+++ src/app/features/setlists/setlist-songs-list/setlist-songs-list.component.ts
@@ -1,50 +1,77 @@
-import { SelectionModel } from '@angular/cdk/collections';
-import { Component, OnInit } from '@angular/core';
-import { MatDialog as MatDialog } from '@angular/material/dialog';
-import { MatTableDataSource as MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Select, Store } from '@ngxs/store';
-import { NGXLogger } from 'ngx-logger';
-import { Observable, concat, first } from 'rxjs';
-import { Account } from 'src/app/core/model/account';
-import { SetlistSong } from 'src/app/core/model/setlist-song';
-import { Song } from 'src/app/core/model/song';
-import { BaseUser, UserHelper } from 'src/app/core/model/user';
-import { AuthenticationService } from 'src/app/core/services/auth.service';
-import { SetlistSongsService } from 'src/app/core/services/setlist-songs.service';
-import { SetlistService } from 'src/app/core/services/setlist.service';
-import { SongService } from 'src/app/core/services/song.service';
-import { AccountState } from 'src/app/core/store/account.state';
-import { LyricAddDialogComponent } from '../../lyrics/lyric-add-dialog/lyric-add-dialog.component';
-import { Lyric } from 'src/app/core/model/lyric';
-import { NgIf, NgClass } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSortModule } from '@angular/material/sort';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { FlexLayoutModule } from 'ngx-flexible-layout';
-import {CdkDragDrop, DragDropModule} from '@angular/cdk/drag-drop';
-import { drop } from 'lodash-es';
+import { SelectionModel } from "@angular/cdk/collections";
+import { Component, OnInit } from "@angular/core";
+import { MatDialog as MatDialog } from "@angular/material/dialog";
+import {
+  MatTableDataSource as MatTableDataSource,
+  MatTableModule,
+} from "@angular/material/table";
+import { Title } from "@angular/platform-browser";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Select, Store } from "@ngxs/store";
+import { NGXLogger } from "ngx-logger";
+import { Observable, concat, first } from "rxjs";
+import { Account } from "src/app/core/model/account";
+import { SetlistSong } from "src/app/core/model/setlist-song";
+import { Song } from "src/app/core/model/song";
+import { BaseUser, UserHelper } from "src/app/core/model/user";
+import { AuthenticationService } from "src/app/core/services/auth.service";
+import { SetlistSongsService } from "src/app/core/services/setlist-songs.service";
+import { SetlistService } from "src/app/core/services/setlist.service";
+import { SongService } from "src/app/core/services/song.service";
+import { AccountState } from "src/app/core/store/account.state";
+import { LyricAddDialogComponent } from "../../lyrics/lyric-add-dialog/lyric-add-dialog.component";
+import { Lyric } from "src/app/core/model/lyric";
+import { NgIf, NgClass } from "@angular/common";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSortModule } from "@angular/material/sort";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { FlexLayoutModule } from "ngx-flexible-layout";
+import { CdkDragDrop, DragDropModule } from "@angular/cdk/drag-drop";
+import { drop } from "lodash-es";
 
 @Component({
-    selector: 'app-setlist-songs-list',
-    templateUrl: './setlist-songs-list.component.html',
-    styleUrls: ['./setlist-songs-list.component.scss'],
-    standalone: true,
-    imports: [FlexLayoutModule, MatCardModule, MatToolbarModule, FormsModule, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatButtonModule, MatIconModule, NgIf, NgClass, DragDropModule]
+  selector: "app-setlist-songs-list",
+  templateUrl: "./setlist-songs-list.component.html",
+  styleUrls: ["./setlist-songs-list.component.scss"],
+  standalone: true,
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    MatToolbarModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatButtonModule,
+    MatIconModule,
+    NgIf,
+    NgClass,
+    DragDropModule,
+  ],
 })
 export class SetlistSongsListComponent {
-  @Select(AccountState.selectedAccount) 
+  @Select(AccountState.selectedAccount)
   selectedAccount$!: Observable<Account>;
   currentUser: BaseUser;
-  displayedSongColumns: string[] = [ 'name', 'artist'];
-  displayedColumns: string[] = [ 'sequence','name', 'artist', 'genre', 'key', 'tempo', 'timeSignature', 'songLength', 'lyrics'];
-  dsSetlistSongs =  new MatTableDataSource<SetlistSong>();
+  displayedSongColumns: string[] = ["name", "artist"];
+  displayedColumns: string[] = [
+    "sequence",
+    "name",
+    "artist",
+    "genre",
+    "key",
+    "tempo",
+    "timeSignature",
+    "songLength",
+    "lyrics",
+  ];
+  dsSetlistSongs = new MatTableDataSource<SetlistSong>();
   dsSongs = new MatTableDataSource<Song>();
   accountId?: string;
   setlistId?: string;
@@ -56,9 +83,9 @@ export class SetlistSongsListComponent {
 
   //Use to select the row.
   selectedRowSequence = -1;
-  
-  
-  constructor(private logger: NGXLogger,
+
+  constructor(
+    private logger: NGXLogger,
     private route: ActivatedRoute,
     private titleService: Title,
     private setlistSongsService: SetlistSongsService,
@@ -66,141 +93,150 @@ export class SetlistSongsListComponent {
     private store: Store,
     private authService: AuthenticationService,
     private router: Router,
-    public dialog: MatDialog) {
-
+    public dialog: MatDialog
+  ) {
     this.authService.user$.subscribe((user) => {
       if (user && user.uid) {
         this.currentUser = UserHelper.getForUpdate(user);
       }
     });
 
-    const selectedAccount = this.store.selectSnapshot(AccountState.selectedAccount);
-    const accountId = this.route.snapshot.paramMap.get('accountid');
-    const setlistId = this.route.snapshot.paramMap.get('setlistid');
-    if(accountId && setlistId){
+    const selectedAccount = this.store.selectSnapshot(
+      AccountState.selectedAccount
+    );
+    const accountId = this.route.snapshot.paramMap.get("accountid");
+    const setlistId = this.route.snapshot.paramMap.get("setlistid");
+    if (accountId && setlistId) {
       this.accountId = accountId;
 
       //Get the songs for the song picker
       this.songService.getSongs(this.accountId).subscribe((songs) => {
-        this.dsSongs =  new MatTableDataSource(songs);
+        this.dsSongs = new MatTableDataSource(songs);
       });
 
       //Get the setlist songs
-      if(setlistId){
+      if (setlistId) {
         this.setlistId = setlistId;
-        this.setlistSongsService.getSetlistSongs(this.accountId, this.setlistId).subscribe((setlistSongs) => {
-          this.dsSetlistSongs =  new MatTableDataSource(setlistSongs);
-          this.setlistSongCount = this.dsSetlistSongs.filteredData.length;
-        });
+        this.setlistSongsService
+          .getSetlistSongs(this.accountId, this.setlistId)
+          .subscribe((setlistSongs) => {
+            this.dsSetlistSongs = new MatTableDataSource(setlistSongs);
+            this.setlistSongCount = this.dsSetlistSongs.filteredData.length;
+          });
       }
     }
-   }
+  }
 
-   //Events ////////////////
-   //Adds a song after the selected row. If no row is selected
+  //Events ////////////////
+  //Adds a song after the selected row. If no row is selected
   onAddSetlistSong(row: Song, sequenceNumberToInsert?: Number): void {
     const sequenceNumber = this.getSequenceNumberForAddOrUpdate();
-    const setlistSong = {displaySequenceNumber: sequenceNumber, sequenceNumber: sequenceNumber, songId: row.id!, isBreak: false, ...row};
-    this.setlistSongsService.addSetlistSong(this.accountId!, this.setlistId!, setlistSong, this.currentUser)
+    const setlistSong = {
+      displaySequenceNumber: sequenceNumber,
+      sequenceNumber: sequenceNumber,
+      songId: row.id!,
+      isBreak: false,
+      ...row,
+    };
+    this.setlistSongsService.addSetlistSong(
+      this.accountId!,
+      this.setlistId!,
+      setlistSong,
+      this.currentUser
+    );
   }
 
-  onAddBreak(){
+  onAddBreak() {
     const sequenceNumber = this.getSequenceNumberForAddOrUpdate();
-    const setlistSong = {sequenceNumber: sequenceNumber, songId: '', isBreak: true, name: 'Break'};
-    this.setlistSongsService.addSetlistBreak(this.accountId!, this.setlistId!, setlistSong)
+    const setlistSong = {
+      sequenceNumber: sequenceNumber,
+      songId: "",
+      isBreak: true,
+      name: "Break",
+    };
+    this.setlistSongsService.addSetlistBreak(
+      this.accountId!,
+      this.setlistId!,
+      setlistSong
+    );
   }
 
-  onEditSong(row): void{
+  onEditSong(row): void {}
 
-  }
-
-  onViewLyrics(event, row: any){
+  onViewLyrics(event, row: any) {
     event.preventDefault();
-    this.router.navigate([`${row.songId}/lyrics`], {relativeTo: this.route});
+    this.router.navigate([`${row.songId}/lyrics`], { relativeTo: this.route });
   }
 
-  onAddLyric(event, row: Song){
+  onAddLyric(event, row: Song) {
     event.preventDefault();
-    const accountLyric = { accountId: this.accountId, songId: row.id, createdByUserId: this.currentUser.uid };
+    const accountLyric = {
+      accountId: this.accountId,
+      songId: row.id,
+      createdByUserId: this.currentUser.uid,
+    };
     const dialogRef = this.dialog.open(LyricAddDialogComponent, {
-      data: {accountLyric: accountLyric, countOfLyrics: 0},
+      data: { accountLyric: accountLyric, countOfLyrics: 0 },
       panelClass: "dialog-responsive",
     });
 
     dialogRef.afterClosed().subscribe((result: Lyric) => {
-      if(result){
-        this.router.navigate([row.id + `/lyrics/${result.id}/edit`], { relativeTo: this.route });
+      if (result) {
+        this.router.navigate([row.id + `/lyrics/${result.id}/edit`], {
+          relativeTo: this.route,
+        });
       }
     });
   }
 
-  onListDrop(event: CdkDragDrop<SetlistSong[]>){
+  onListDrop(event: CdkDragDrop<SetlistSong[]>) {
     const droppedSetlistSong = event.item.data as SetlistSong;
     const sequenceNumberToInsert = event.currentIndex;
-    
-    if(event.previousIndex > event.currentIndex){
-      const subscription = this.setlistSongsService.insertSetlistSong(droppedSetlistSong, sequenceNumberToInsert + 1, this.accountId!, this.setlistId!, this.currentUser)
-                              .pipe(first())
-                              .subscribe()
-      
-    }
-    
-    
-  }
-
-  //TODO: remove this function if I don't use it. 
-  //Helper functions
-  //setlistSongToMove is the SetlistSong to move
-  //sequenceNumberToInsert is a 1 based sequence not 0
-  moveSetlistSongUpTo(setlistSongToMove: SetlistSong, sequenceNumberToInsert: number): void {
-    const setlistSongToDisplace = this.dsSetlistSongs.data[sequenceNumberToInsert - 1];
-    const setlistSongToInsert = {...setlistSongToMove, songId: setlistSongToMove.id! };
-
-    if(setlistSongToDisplace?.sequenceNumber){
-      //first increment the sequence of the song that is displaced.
-      setlistSongToDisplace.sequenceNumber += .01;
-      //Set the sequence number for the song to insert
-      setlistSongToInsert.sequenceNumber = setlistSongToDisplace.sequenceNumber - .01;
-      
-      concat( 
-        //Update the displaced song
-        this.setlistSongsService.updateSetlistSong(setlistSongToDisplace?.id!, this.accountId!, this.setlistId!, setlistSongToDisplace, this.currentUser),
-        //Update the song to insert
-        this.setlistSongsService.updateSetlistSong(setlistSongToInsert.id!, this.accountId!, this.setlistId!, setlistSongToInsert, this.currentUser)
-      );
+    if (event.previousIndex > event.currentIndex) {
+      const subscription = this.setlistSongsService
+        .moveUpSetlistSong(
+          droppedSetlistSong,
+          sequenceNumberToInsert + 1,
+          this.accountId!,
+          this.setlistId!,
+          this.currentUser
+        )
+        .pipe(first())
+        .subscribe();
     }
   }
 
-  getSequenceNumber(rowIndex: number){
+  getSequenceNumber(rowIndex: number) {
     //Reset the break count with the first row.
-    if(rowIndex === 0){
+    if (rowIndex === 0) {
       this.setlistBreakCount = 0;
     }
-    
-    if(this.setlistBreakCount > 0){
+
+    if (this.setlistBreakCount > 0) {
       this.displaySequence = rowIndex - this.setlistBreakCount;
-    }
-    else{
+    } else {
       this.displaySequence = rowIndex;
     }
 
     return this.displaySequence + 1;
   }
 
-  incrementSetBreakCount(rowIndex: number){
+  incrementSetBreakCount(rowIndex: number) {
     //Reset the break count with the first row.
-    if(rowIndex === 0){
+    if (rowIndex === 0) {
       this.setlistBreakCount = 0;
     }
     this.setlistBreakCount += 1;
-    return '';
+    return "";
   }
 
-  selectRow(row){
-    if(this.selectedRowSequence === -1 || this.selectedRowSequence !== row.sequenceNumber){
+  selectRow(row) {
+    if (
+      this.selectedRowSequence === -1 ||
+      this.selectedRowSequence !== row.sequenceNumber
+    ) {
       this.selectedRowSequence = row.sequenceNumber;
-    }
-    else{
+    } else {
       this.selectedRowSequence = -1;
     }
   }
