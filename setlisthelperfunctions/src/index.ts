87c674df35bc1fd05971a69e8d954fbb42606a8d
Tim Burnham <timburnham73@gmail.com>
2024-02-28 22:06:52 -0500

    fixing accounts alignment and change songs toolbar buttons.

---
+++ setlisthelperfunctions/src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const functions = require('firebase-functions');
+const logger = require('firebase-functions/logger');
+const onRequest = require('firebase-functions/v2/https');
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
