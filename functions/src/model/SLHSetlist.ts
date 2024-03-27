2817d526fc42602977824955de73f34d2be65f3e
Tim Burnham <tim.burnham@broadcom.com>
2024-03-27 18:03:29 -0400

    Adding code to import setlists

--- functions/src/model/SLHSetlist.ts
+++ functions/src/model/SLHSetlist.ts
@@ -1,3 +1,7 @@
+import { Timestamp } from "firebase-admin/firestore"
+import { BaseUser, UserHelper } from "./user"
+import { Setlist } from "./setlist"
+
 export interface SLHSetlist {
     Deleted: boolean
     Deprecated: boolean
@@ -12,3 +16,28 @@ export interface SLHSetlist {
     LastEdit: string
     songs: number[]
 }
+
+export class SLHSetlistHelper {
+    public static getSongLengthMinSec(SongLength) {
+      return {
+        minutes: Math.floor(SongLength / 60),
+        seconds: SongLength % 60
+      };
+    }
+
+    static slhSetlistToSetlist(slhSetlist: SLHSetlist, editingUser: BaseUser): Setlist {
+      const nowTimestamp = Timestamp.now();
+      const gigDate = this.getSongLengthMinSec(slhSetlist.);
+      return {
+        name: slhSetlist.Name ?? "",
+        gigDate: Timestamp.now(),//TODO: Fix git date in the setlist helper function
+        gigLocation: slhSetlist.GigLocation ?? "",
+        makePublic: slhSetlist.MakePublic,
+        deprecated: slhSetlist.Deprecated,
+        lastEdit: nowTimestamp,
+        lastUpdatedByUser : UserHelper.getForUpdate(editingUser),
+        dateCreated: nowTimestamp,
+        createdByUser: UserHelper.getForUpdate(editingUser),
+        
+      };
+    }
---
+++ functions/src/model/setlist-break.ts
@@ -0,0 +1,26 @@
+
+import { Timestamp } from "firebase-admin/firestore";
+import { Base } from "./base";
+import { BaseUser, UserHelper } from "./user";
+
+export interface SetlistBreak extends Base {
+   sequenceNumber: number;
+   songId: string;
+   isBreak: boolean;
+   name: string;
+}
+
+export class SetlistBreakHelper {
+   static getSetlistBreakForAddOrUpdate(setlistBreak: Partial<SetlistBreak>, editingUser: BaseUser): SetlistBreak {
+      return {
+         sequenceNumber: setlistBreak.sequenceNumber ?? 1,
+         songId: setlistBreak.songId ?? "",
+         isBreak: setlistBreak.isBreak ?? false,
+         name: setlistBreak.name ?? '',
+         lastEdit: Timestamp.fromDate(new Date()),
+         lastUpdatedByUser: UserHelper.getForUpdate(editingUser),
+         dateCreated: setlistBreak.dateCreated ?? Timestamp.fromDate(new Date()),
+         createdByUser: setlistBreak.createdByUser ?? UserHelper.getForUpdate(editingUser),
+      };
+   }
+}
